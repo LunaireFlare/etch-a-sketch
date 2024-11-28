e90e7081774b4192d581fa87e3c1c61f6ae9a1f9
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2024-11-28 10:55:26 +0100

    build: refactor createGrid() + add changeGrid()
    Refactored createGrid() function so it would take a size parameter and
    hard-coded maximum grid size at 100, rendering the global constants
    superfluous.
    
    Added a changeGrid() function to reset size of grid based on user's
    input in prompt and moved hoverColor() function call inside createGrid()
    so it wouldn't disappear upon changing grid size.

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 const gridContainer = document.querySelector(".grid-container");
+const btnGridSize = document.querySelector(".btn-gridsize");
 
-const maximumSize = 100;
-const userSize = 16;
-
-
-function createGrid() {
-  const cellSize = maximumSize / userSize; 
-
-  for (let i = 0; i < userSize * userSize; i++) {
+function createGrid(defaultSize) {
+  for (let i = 0; i < defaultSize * defaultSize; i++) {
+    const cellSize = 100 / defaultSize;
     const gridCell = document.createElement("div");
-    gridCell.style.width = `${cellSize}%`;
-    gridCell.style.height = `${cellSize}%`;
     gridCell.classList.add("grid-cell");
     gridContainer.appendChild(gridCell);
+    gridCell.style.width = `${cellSize}%`;
+    gridCell.style.height = `${cellSize}%`;
   }
-}
+  hoverColor()
+};
 
 function hoverColor() {
   const gridCells = document.querySelectorAll(".grid-cell");
   gridCells.forEach((cell) => {
     cell.addEventListener("mouseover", () => {
-      cell.style.backgroundColor = "coral";
+      cell.style.backgroundColor = "palevioletred";
     });
   });
-}
+};
 
-createGrid();
-hoverColor();
+function changeGrid(defaultSize) {
+    let newSize = prompt("Choose the number of squares per side", "Max. 100");
+    if(newSize > 0 && newSize <= 100) {
+      gridContainer.textContent = "";
+      createGrid(newSize);
+    } else {
+      prompt("Please enter a valid number.");
+    };
+};
+
+
+btnGridSize.addEventListener("click", changeGrid);
+
+createGrid(16);
